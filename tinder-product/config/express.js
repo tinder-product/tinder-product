27a18314da776e8f4540fec65ebcd0e15380d691
trigoporres <santiagotrigoporres@gmail.com>
2017-09-18 08:50:01 +0200

    Problemas

--- tinder-product/config/express.js
+++ tinder-product/config/express.js
@@ -5,7 +5,6 @@ const logger         = require('morgan');
 const cookieParser   = require('cookie-parser');
 const bodyParser     = require('body-parser');
 const mongoose       = require('mongoose');
-const passport       = require("passport");
 
 module.exports = function (app){
 
